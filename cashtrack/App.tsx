cf60d03e7200b2665649088407d2c7ae4d0f4057
Fadillah Akbar <faoedill@gmail.com>
2026-01-17 21:30:48 +0700

    add frontend with expo

---
+++ cashtrack/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Cash Track</Text>
+
+      <View style={styles.card}>
+        <Text>Total Balance</Text>
+        <Text style={styles.amount}>$0.00</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  card:{
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    shadowColor: '#000',
+  },
+  amount:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+  }
+});
